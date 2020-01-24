20ffcaa94532fa3e14ad0a4ec0b362656a2f7cce
JonSmebye <jonsmebye@gmail.com>
2020-01-24 15:55:02 +0100

    Face detecter and img resize

---
+++ faceDetection.py
@@ -0,0 +1,59 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+    help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+    help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+ 
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+ 
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))
+
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+ 
+        if confidence < args["confidence"]:
+            continue
+
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        face = frame[startY:endY,startX:endX]
+# show the output frame
+    key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("frame.jpg", face)
+    time.sleep(5.0)
+ 
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
