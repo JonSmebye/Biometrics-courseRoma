2a832fae9cad96e3ec57ae373ce0755b966c7022
JonSmebye <jonsmebye@gmail.com>
2020-01-27 17:04:38 +0100

    Ryddet opp

--- faceDetection.py
+++ faceDetection.py
@@ -1,52 +1,78 @@
+import face_recognition
 from imutils.video import VideoStream
-import numpy as np
-import argparse
-import imutils
-import time
 import cv2
- 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-    help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-    help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-    help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
+import time
+import os
 
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
- 
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+def getImageFromWebcam():
+	cam = VideoStream(src=0).start()
+	time.sleep(0.5)
+	image = cam.read()
+	cv2.destroyAllWindows()
+	cam.stop()
+	return image
 
-while True:
+def faceRecognition(imgPath, img):
+	known_image = face_recognition.load_image_file(imgPath)
+	unkown_image = img
+	know_encoding = face_recognition.face_encodings(known_image)[0]
+	unkwnown_encoding = face_recognition.face_encodings(unkown_image)[0]
+	results = face_recognition.compare_faces([know_encoding],unkwnown_encoding)
+	return results[0]
 
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
- 
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-        (300, 300), (104.0, 177.0, 123.0))
+def takePictureAsRefferance():
+	image = getImageFromWebcam()
+	filePath = "refe.jpg"
+	cv2.imwrite(filePath, image)
+	return filePath
 
-    net.setInput(blob)
-    detections = net.forward()
+def menu():
+	ans=True
+	while ans:
+		print ("""
+	1.Take referance picture now
+	2.Use an existing photo
+			""")
+		ans=input("What would you like to do? ") 
+		if ans=="1":
+			print("\nTaking a picture now")
+			imagePath = takePictureAsRefferance()
+			ans = False
+		elif ans=="2":
+			imagePath = input("What is the entire filepath of the picture?")
+			ans = False
+		else:
+			print("\nNot Valid Choice Try again")
+	return imagePath
 
-    for i in range(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
- 
-        if confidence < args["confidence"]:
-            continue
-
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
-
-        face = frame[startY:endY,startX:endX]
-
-    key = cv2.waitKey(1) & 0xFF
-    cv2.imwrite("frame.jpg", face)
-    time.sleep(5.0)
- 
-cv2.destroyAllWindows()
-vs.stop()
+def main():
+	imagePathStored = menu()
+	recognized = 0
+	notJon = 0
+	notRecognized = 0
+	while True:
+		img = getImageFromWebcam()
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		face_front_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+		faces_front = face_front_cascade.detectMultiScale(gray, 1.3, 5)
+		cv2.destroyAllWindows()
+		try:
+			faces_front.all()
+			res = faceRecognition(imagePathStored, img)
+			if res != True:
+				print("Not authorized")
+				notRecognized += 1
+				if notRecognized >4:
+					print("throw out")
+					os.system('launchctl bootout gui/$(id -u "Jon Smebye")')
+			else:
+				print("A Jon appeared")
+				recognized +=1
+				notRecognized = 0
+		except:
+			print("No faces detected")
+		time.sleep(2.0)
+	print("recognized: " + str(recognized))
+	print("Not Jon: " + str(notJon))
+if __name__ == '__main__':
+    main()
